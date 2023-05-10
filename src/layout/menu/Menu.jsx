bc7d2f475105a79375e5130bef3c4aa48356cc82
Stéphane Le Pajolec <stephane.lepajolec@lapiscine.com>
2023-05-10 18:05:59 +0200

    maj menu

--- src/layout/menu/Menu.jsx
+++ src/layout/menu/Menu.jsx
@@ -11,6 +11,9 @@ const Menu = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+
+
+
                     <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                         <div className="navbar-nav d-flex align-items-center">
                             <Link to="/" className={location.pathname === "/" ? "nav-link menu-link actif" : "nav-link menu-link"} aria-current="page" href="#">ACCUEIL</Link>
@@ -39,46 +42,45 @@ const Menu = () => {
                 </button>
             </div> */}
 
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <div className="container d-flex d-row justify-content-between align-items-center">
+                <nav className="navbar navbar-expand-lg menu-navbar">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <Link to="/" className={location.pathname === "/" ? "nav-link menu-link actif" : "nav-link menu-link"} aria-current="page" href="#">ACCUEIL</Link>
                             </li>
+                            <li><i className="fa-solid fa-circle" /></li>
                             <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                                <Link to="/search" className={location.pathname === "/search" ? "nav-link menu-link actif" : "nav-link menu-link"} aria-current="page" href="#">RECHERCHE AVANCÉE</Link>
                             </li>
+                            <li><i className="fa-solid fa-circle" /></li>
                             <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                                <a className="nav-link menu-link" href="#">MON ESPACE</a>
                             </li>
+                            <li><i className="fa-solid fa-circle" /></li>
                             <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
+                                <a className="nav-link menu-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                ADMIN
                             </a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                <li><a className="dropdown-item menu-link menu-link-dropdown" href="#">TYPE DE LOISIR</a></li>
+                                <li><a className="dropdown-item menu-link menu-link-dropdown" href="#">LOCALISATION</a></li>
+                                <li><a className="dropdown-item menu-link menu-link-dropdown" href="#">CENTRES</a></li>
                             </ul>
                             </li>
                         </ul>
-                        </div>
                     </div>
                 </nav>
-
-
+                <button className="d-flex align-items-center btn-connect">
+                    <i className="fa-solid fa-user"></i>
+                    <span>Connexion</span>
+                </button>
+            </div>
 
         </section>
-
-
-
-
-
     )
 }
 
