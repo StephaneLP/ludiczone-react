ed585b4e91174a6ba26037e43f2cff6d2972649b
StephaneLP <stephanelepajolec@outlook.com>
2023-05-15 23:16:33 +0200

    maj

--- src/pages/admin/AdminAreaTypeCreate.jsx
+++ src/pages/admin/AdminAreaTypeCreate.jsx
@@ -13,31 +13,31 @@ const AdminAreaTypeCreate = () => {
         <section className="container-fluid admin">
             <h1>Ajouter un type de loisir</h1>
             <div className="container">
-                <form onSubmit={handleSubmit}>
+                <form className="admin-create" onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-12 col-md-2"></div>
-                        <div className="col-12 col-md-4" style={{backgroundColor: "blue"}}>
+                        <div className="col-12 col-md-4">
                             <div>
                                 <label>
                                     <span className="label-libelle">Nom</span>
-                                    <input type="text" name="name" />
+                                    <input type="text" name="name" maxLength="51" placeholder="nom" size="50" />
                                 </label>                       
                             </div>
                             <div>
                                 <label>
                                 <span className="label-libelle">Description</span>
-                                    <input type="text" name="description" />
+                                    <textarea name="description" placeholder="description" cols="50" />
                                 </label>                            
                             </div>
                         </div>
-                        <div className="col-12 col-md-4" style={{backgroundColor: "green"}}>
+                        <div className="col-12 col-md-4">
                             image
                         </div>
                         <div className="col-12 col-md-2"></div>
                     </div>
                     <div className="row">
                         <div className="col-12">
-                            <input className="button" type="submit" value="Créer" />
+                            <input className="button" type="submit" disabled value="Créer" />
                         </div>
                     </div>
                 </form>
