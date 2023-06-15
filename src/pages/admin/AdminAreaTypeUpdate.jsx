cd7218828e405f56f9c7a4af0bc4798f59f34a1d
StephaneLP <stephanelepajolec@outlook.com>
2023-06-15 09:39:44 +0200

    maj

--- src/pages/admin/AdminAreaTypeUpdate.jsx
+++ src/pages/admin/AdminAreaTypeUpdate.jsx
@@ -48,6 +48,7 @@ const AdminAreaTypeUpdate = () => {
                     setGetAreaType(res.data)
                 }
                 else {
+                    console.log("STOP")
                     navigate('/admin-area-type',{
                         state: {
                             alter: {
