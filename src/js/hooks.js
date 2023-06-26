b5ed81b2847b102d3d448f287300733992acfb63
Stéphane Le Pajolec <stephane.lepajolec@lapiscine.com>
2023-06-26 18:57:12 +0200

    maj

--- src/js/hooks.js
+++ src/js/hooks.js
@@ -1,5 +1,5 @@
 /* Import des fonctions, variables & images */
-import { checkStatus } from "./utils.js"
+import { cleanLocalStorage, checkStatus } from "./utils.js"
 
 /* Import des Hooks & composants react-rooter */
 import { useEffect } from "react"
@@ -28,20 +28,17 @@ const useCheckIsAdmin = (token, route) => {
                 si status <> 200 alors redirection */
                 navParams = {...checkStatus(res.status, route)}
                 if(navParams.route !== "") {
-                    localStorage.removeItem("jwt")
-                    localStorage.removeItem("pseudo")
+                    cleanLocalStorage()
                     navigate(navParams.route,{state: navParams.state})
                 }
             })
             .catch((error) => {
-                localStorage.removeItem("jwt")
-                localStorage.removeItem("pseudo")
+                cleanLocalStorage()
                 navigate('/erreur',{state: {message: error.message}})
             })
         }
         else { // Aucun token n'est présent dans le localStorage
-            localStorage.removeItem("jwt")
-            localStorage.removeItem("pseudo")
+            cleanLocalStorage()
             navigate('/erreur',{state: {message: "Vous n'avez pas les droits requis. Veuillez vous identifier S.V.P."}})
         }
     },[token, navigate, route])
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,8 +1,10 @@
+/* Couleurs des messages affichés suite à la réponse d'une requête */
 const colorMsg = {
     success: "#77a366",
     error: "#b65d5d",
 }
 
+/* Formatage des dates */
 const formatDate = (dateString, mode) => {
     const optionsShort = { year: "numeric", month: "long", day: "numeric" }
     const optionsLong = { weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" }
@@ -10,6 +12,20 @@ const formatDate = (dateString, mode) => {
     return new Date(dateString).toLocaleDateString("fr-FR", (mode === "short" ? optionsShort : optionsLong))
 }
 
+/* Les données enregistrées dans le localStorage lors du login sont effacées */
+const cleanLocalStorage = () => {
+    localStorage.removeItem("jwt")
+    localStorage.removeItem("pseudo")
+}
+
+/*********************************************************
+Fonction qui retourne une route et des paramètres selon le statut,
+pour une éventuelle redirection si erreur :
+- 401 : authentification => page reconnexion
+- 403 : droits => page erreur
+- 500 : erreur interne serveur => page erreur
+- autre (200) : pas de redirection
+*********************************************************/
 const checkStatus = (status, route) => {
     let navParams = {}
 
@@ -19,8 +35,6 @@ const checkStatus = (status, route) => {
             navParams.state =  {reconnect: true, route: route}
             break
         case 403:
-            localStorage.removeItem("jwt")
-            localStorage.removeItem("pseudo")
             navParams.route = "/erreur"
             navParams.state =  {message: "Vous n'avez pas les droits requis. Veuillez vous reconnecter S.V.P."}
             break
@@ -36,4 +50,4 @@ const checkStatus = (status, route) => {
     return navParams
 }
 
-export { colorMsg, formatDate, checkStatus }
+export { colorMsg, formatDate, cleanLocalStorage, checkStatus }
